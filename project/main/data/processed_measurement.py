fdf6aa9212ea7a4fbcfb7f185c3b19c26592d1dc
Gerson Montesinos <g.montesinos@qairadrones.com>
2021-02-03 14:02:36 -0500

    t

--- project/main/data/processed_measurement.py
+++ project/main/data/processed_measurement.py
@@ -16,9 +16,9 @@ def getProcessedData():
     qhawax_name = request.args.get('name')
     try:
         interval_minutes = int(request.args.get('interval_minutes')) \
-            if request.args.get('interval_minutes') is not None else 60 
+            if request.args.get('interval_minutes') is not None else 60
         final_timestamp = datetime.datetime.now(dateutil.tz.tzutc())
-        initial_timestamp = final_timestamp - datetime.timedelta(minutes=interval_minutes) 
+        initial_timestamp = final_timestamp - datetime.timedelta(minutes=interval_minutes)
         processed_measurements = get_data_helper.queryDBProcessed(qhawax_name, initial_timestamp, final_timestamp)
         if processed_measurements is not None:
             return make_response(jsonify(processed_measurements), 200)
@@ -41,10 +41,10 @@ def handleProcessedData():
         data_json = util_helper.validAndBeautyJsonProcessed(data_json)
         post_data_helper.storeProcessedDataInDB(data_json)
         data_json['ID'] = product_id
-        data_json['zone'] = "Zona No Definida"
+        data_json['zone'] = "Undefined Zone"
         mode = same_helper.getQhawaxMode(product_id)
         inca_value = same_helper.getMainIncaQhawaxTable(product_id)
-        if(mode == "Cliente" and inca_value!=None):
+        if(mode == "Customer" and inca_value!=None):
             data_json['zone'] = get_business_helper.getNoiseData(product_id)
             minutes_difference,last_time_turn_on = get_business_helper.getHoursDifference(product_id)
             if(minutes_difference!=None):
@@ -59,3 +59,18 @@ def handleProcessedData():
         json_message = jsonify({'error': '\'%s\'' % (e)})
         return make_response(json_message, 400)
 
+
+@app.route('/api/processed_measurements_andean_drone/', methods=['GET'])
+def getProcessedDataFromAndeanDrone():
+    """ To list all measurement of processed measurement table record the last N minutes """
+    qhawax_name = request.args.get('qhawax_name')
+    initial_timestamp = datetime.datetime.strptime(request.args.get('initial_timestamp'), '%d-%m-%Y %H:%M:%S')
+    final_timestamp = datetime.datetime.strptime(request.args.get('final_timestamp'), '%d-%m-%Y %H:%M:%S')
+    try:
+        processed_measurements = get_data_helper.queryDBProcessed(qhawax_name, initial_timestamp, final_timestamp)
+        if processed_measurements is not None:
+            return make_response(jsonify(processed_measurements), 200)
+        return make_response(jsonify('Measurements not found'), 200)
+    except TypeError as e:
+        json_message = jsonify({'error': '\'%s\'' % (e)})
+        return make_response(json_message, 400)
