448f2f7451c9f6af20987294e7d1e9971be6ee3f
SabCampos <s.campos@qairadrones.com>
2021-02-11 12:53:10 -0500

    script drone

--- scripts/droneSimulation.py
+++ scripts/droneSimulation.py
@@ -3,8 +3,11 @@ import time
 import sys
 import random
 import math
+import dateutil.parser
+import dateutil.tz
+import datetime
 
-# python scripts/telemetrySimulation.py "qH006" 20 1 7
+# python scripts/droneSimulation.py "qH006" 20 1 7
 
 
 # BASE_URL = 'https://openqairamapnapi.qairadrones.com/'
@@ -12,6 +15,29 @@ BASE_URL = 'http://0.0.0.0:5000/'
 SEND_TELEMETRY= BASE_URL + '/api/send_telemetry_andean_drone/'
 COMPLETE_FLIGHT =BASE_URL +'/api/complete_flight'
 START_FLIGHT = BASE_URL + 'api/record_start_flight'
+PROCESSED_MEASUREMENT= BASE_URL + '/api/dataProcessed/'
+
+measurement_qhawax = {
+	"ID":None,
+	"timestamp":None,
+	"lat": -12.07070,
+	"lon": -77.08052,
+	"CO":42.363,
+	"H2S":0,
+	"NO2":0,
+	"O3":0,
+	"SO2":0,
+	"PM1":4.775,
+	"PM25":12.631,
+	"PM10":25.046,
+	"UV":0,
+	"UVA":0,
+	"UVB":0,
+	"spl":0,
+	"temperature":23.0,
+	"pressure":100629.10,
+	"humidity":3
+	}
 
 telemetry_qhawax ={
 				"token": "droneandino123",
@@ -47,8 +73,9 @@ telemetry_qhawax ={
 					"irlock": None
 					}
 				}
+
 complete_flight = {
-    "flight_end": "2021-01-27 13:05:00",
+    "flight_end": str(datetime.datetime.now(dateutil.tz.tzutc())),
     "qhawax_name": "qH006",
     "flight_detail": "Terrible flight",
     "location": {
@@ -58,7 +85,7 @@ complete_flight = {
 }
 
 start_flight = {
-    "flight_start": "2021-01-27 13:00:00",
+    "flight_start": str(datetime.datetime.now(dateutil.tz.tzutc())),
     "qhawax_name": "qH006"
 }
 
@@ -69,18 +96,30 @@ offset =int(sys.argv[4])
 dLat = offset/R
 
 
+
 if(len(sys.argv)==5):
 	start_flight['qhawax_name']=sys.argv[1]
+	print(start_flight)
 	response_telemetry = requests.post(START_FLIGHT, json=start_flight)
 
 	for index in range(times_iterate):
+		measurement_qhawax["ID"]=sys.argv[1]
+		measurement_qhawax["timestamp"]= str(datetime.datetime.now(dateutil.tz.tzutc()))
+		measurement_qhawax["O3"]= random.randrange(times_iterate)
+		measurement_qhawax["H2S"]= random.randrange(times_iterate)
+		measurement_qhawax["humidity"]= random.randrange(times_iterate-index)
+		measurement_qhawax["UV"]= random.randrange(15)
+		lat = measurement_qhawax["lat"]
+		lon = measurement_qhawax["lon"]
+		dLon = offset/(R*math.cos(math.pi*lat/180))
+		measurement_qhawax["lat"]= float(lat)+dLat*180/math.pi
+		measurement_qhawax["lon"]= float(lon)+dLon*180/math.pi
+		print(measurement_qhawax)
+		response_measurement = requests.post(PROCESSED_MEASUREMENT, json=measurement_qhawax)
 		telemetry_qhawax["room"]=sys.argv[1]
 		telemetry_qhawax["telemetry"]["alt"]= random.randrange(times_iterate)
 		telemetry_qhawax["telemetry"]["voltage"]= random.randrange(times_iterate)
 		telemetry_qhawax["telemetry"]["airspeed"]= random.randrange(times_iterate-index)
-		lat = telemetry_qhawax["telemetry"]["lat"]
-		lon = telemetry_qhawax["telemetry"]["lon"]
-		dLon = offset/(R*math.cos(math.pi*lat/180))
 		telemetry_qhawax["telemetry"]["lat"]= float(lat)+dLat*180/math.pi
 		telemetry_qhawax["telemetry"]["lon"]= float(lon)+dLon*180/math.pi
 		print(telemetry_qhawax)
@@ -90,4 +129,5 @@ if(len(sys.argv)==5):
 	complete_flight['qhawax_name']=sys.argv[1]	
 	complete_flight['location']['lat']=telemetry_qhawax["telemetry"]["lat"]
 	complete_flight['location']['lon']=telemetry_qhawax["telemetry"]["lon"]
+	print(complete_flight)
 	response_telemetry = requests.post(COMPLETE_FLIGHT, json=complete_flight)
--- scripts/staticQhawaxSimulation.py
+++ scripts/staticQhawaxSimulation.py
@@ -10,10 +10,28 @@ import random
 BASE_URL = 'https://qairamapnapi.qairadrones.com/'
 PROCESSED_MEASUREMENT= BASE_URL + '/api/dataProcessed/'
 
-# python staticQhawaxSimulation.py "qH002" 20 1
-measurement_qhawax = {"ID":None,"timestamp":None,"lat":-12.132288,"lon":-77.027702,"CO":42.363,"H2S":0,"NO2":0,\
-					  "O3":0,"SO2":0,"PM1":4.775,"PM25":12.631,"PM10":25.046,"UV":0,"UVA":0,"UVB":0,"spl":0,\
-					  "temperature":23.0,"pressure":100629.10,"humidity":3}
+# python scripts/staticQhawaxSimulation.py "qH002" 20 1
+measurement_qhawax = {
+	"ID":None,
+	"timestamp":None,
+	"lat":-12.132288,
+	"lon":-77.027702,
+	"CO":42.363,
+	"H2S":0,
+	"NO2":0,
+	"O3":0,
+	"SO2":0,
+	"PM1":4.775,
+	"PM25":12.631,
+	"PM10":25.046,
+	"UV":0,
+	"UVA":0,
+	"UVB":0,
+	"spl":0,
+	"temperature":23.0,
+	"pressure":100629.10,
+	"humidity":3
+	}
 #times = 10
 sleep_time_seconds=int(sys.argv[3])
 times_iterate= int(sys.argv[2])
