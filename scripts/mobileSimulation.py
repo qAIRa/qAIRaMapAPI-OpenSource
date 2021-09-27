391455cafeaa562f6fe1e27e107ad3347b314470
Gerson Montesinos <g.montesinos@qairadrones.com>
2021-09-27 18:46:37 -0500

    a few changes for privacy settings

--- scripts/mobileSimulation.py
+++ scripts/mobileSimulation.py
@@ -10,33 +10,15 @@ import random
 from datetime import date, timedelta, tzinfo
 
 
-# DATA_PROCESSED_ENDPOINT = 'api/dataProcessedMobile/'
-# OPEN_BASE_URL = 'https://openqairamapnapi.qairadrones.com/'
-
-BASE_URL = 'https://qairamapnapi.qairadrones.com/'
-DATA_PROCESSED_TEST = 'api/dataProcessedTest/'
-
-#PROCESSED_MEASUREMENT= OPEN_BASE_URL + DATA_PROCESSED_ENDPOINT
-# produccion
-PROCESSED_MEASUREMENT = BASE_URL + DATA_PROCESSED_TEST
-print(PROCESSED_MEASUREMENT)
-print('https://qairamapnapi.qairadrones.com/api/dataProcessedTest/')
-# CO, CO2, NO2, O3, SO2, H2S, PM25, PM10, VOC  ug_m3
+############# DON'T RUN THIS PLEASE, THANK YOU :)  ######################
 
 #"2021-05-26 12:20:00-05:00"
 #'2021-05-26 17:19:15.560919+00:00'
 
-data_json = {"CO": 10000,"CO_ug_m3": 8000,
-         "H2S_ug_m3": 43.404,"H2S": 43.404,
-         "NO2": 100.78,"NO2_ug_m3": 19.78,
-         "O3": 40.126,"O3_ug_m3": 3.126,"VOC":0,
-         "CO2":1,"SO2": 4.388,"SO2_ug_m3": 4.388,
-         "PM10": 35.349,"PM25": 11.678,"UVA":1,
-         "UVB":1,"alt": 0.0,"lat": -12.0402780000002,
-         "lon": -77.0436090000003,"PM1":1,
-         "timestamp": "2021-05-19 07:56:08-05:00",
-         "ID":"qH022","pressure":10,"humidity":25,
-         "I_temperature":25,"temperature":21,"spl":1,"UV":1}
+data_json = {"CO": 10000,"CO_ug_m3": 8000, "H2S_ug_m3": 43.404,"H2S": 43.404, "NO2": 100.78,"NO2_ug_m3": 19.78,
+         "O3": 40.126,"O3_ug_m3": 3.126,"VOC":0, "CO2":1,"SO2": 4.388,"SO2_ug_m3": 4.388, "PM10": 35.349,"PM25": 11.678,"UVA":1,
+         "UVB":1,"alt": 0.0,"lat": -12.0402780000002,"lon": -77.0436090000003,"PM1":1,"timestamp": "2021-05-19 07:56:08-05:00",
+         "ID":"qH022","pressure":10,"humidity":25,"I_temperature":25,"temperature":21,"spl":1,"UV":1}
 
 # jsonData = {'timestamp': data['timestamp'],
 #             'CO': data['CO'],'CO_ug_m3': data['CO_ug_m3'], 
